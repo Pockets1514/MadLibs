a9a7e72ab2ac4ad534b4407d9a5506e3b5b86afc
Pockets1514 <55607445+Pockets1514@users.noreply.github.com>
2020-04-19 03:22:24 -0600

    styled genereate and clear buttons

--- src/components/card.js
+++ src/components/card.js
@@ -82,7 +82,7 @@ class Card extends Component {
 
                 }
                 </div>
-                <button type='submit'>{this.state.contentVisible ? 'Start Over' : 'Generate MadLib'}</button>
+                <button className ={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type='submit'>{this.state.contentVisible ? 'Start Over' : 'Generate MadLib'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : <div>Get to it!</div>
                 }
